ff784b7d8d961efb41a31e865fe098adbc86aac0
xiaofei sun <xiaofeisunduter@163.com>
2021-06-04 17:11:51 +0800

    Create colorize_utils.py

--- colorize_utils.py
+++ colorize_utils.py
@@ -1,3 +1,15 @@
+def colorize_mask(label, palette):
+    """
+    将label以类别对应颜色保存成彩色图。
+    :param label:[512,1024]
+    :param palette:[19*3] eg.:[0, 0, 0, 128, 0, 0,... 0, 128, 0]
+    """
+    zero_pad = 256 * 3 - len(palette)
+    for i in range(zero_pad):
+        palette.append(0)
+    new_mask = PIL.Image.fromarray(mask.astype(np.uint8)).convert('P')
+    new_mask.putpalette(palette)
+    return new_mask
 def blend_image(self, image, label, palette):
     """
     将label值以对应颜色的形式与image，以指定透明度混合。
