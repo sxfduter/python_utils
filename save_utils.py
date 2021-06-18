4711912b53bf143dba2ebf8d41c9508490cc09b0
xiaofei sun <xiaofeisunduter@163.com>
2021-06-18 21:07:26 +0800

    Update save_utils.py

--- save_utils.py
+++ save_utils.py
@@ -20,3 +20,13 @@ def write_to_txt(data, name="a"):
      with open(name+'.txt', 'w') as f:
          for i in range(len(data)):
              f.write(data[i]+"\r")
+               
+ def read_to_list(file_txt):
+     """
+     将txt读到list
+     :param file_txt: txt文件
+     :return:
+     """
+     data_list = []
+     for line in open(file_txt, 'r'):
+         data_list.append(line[:-1])
