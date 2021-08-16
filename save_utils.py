1fc249b0dbf8479eae2ea2e5116f513e5947e1e1
xiaofei sun <xiaofeisunduter@163.com>
2021-08-16 14:08:38 +0800

    Update save_utils.py

--- save_utils.py
+++ save_utils.py
@@ -31,3 +31,25 @@ def read_to_list(file_txt):
     for line in open(file_txt, 'r'):
         data_list.append(line[:-1])
     return data_list
+
+def get_pic_by_url(folder_path, lists):
+    """
+    将lists中url下载到folder_path本地目录中
+    :param folder_path: 本地目录
+    :return: lists: url list
+    """
+    if not os.path.exists(folder_path):
+        print("Selected folder not exist, try to create it.")
+        os.makedirs(folder_path)
+    for url in lists:
+        print("Try downloading file: {}".format(url))
+        filename = url.split('/')[-1].split('?')[0]
+        filepath = folder_path + '/' + filename
+        if os.path.exists(filepath):
+            print("File have already exist. skip")
+        else:
+            try:
+                urllib.request.urlretrieve(url, filename=filepath)
+            except Exception as e:
+                print("Error occurred when downloading file, error message:")
+                print(e)
